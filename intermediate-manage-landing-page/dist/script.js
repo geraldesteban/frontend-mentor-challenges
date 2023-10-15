a0abf94bf6b13d456fe42c741e08004b5864c78a
geraldesteban <wakin.esteban@gmail.com>
2023-10-15 15:10:20 +0800

    add styles and move input.css to dist

--- intermediate-manage-landing-page/dist/script.js
+++ intermediate-manage-landing-page/dist/script.js
@@ -1,19 +1,20 @@
 "use strict";
 const navUl = document.querySelector(".navbar-ul");
-const navOpen = document.querySelector(".navbar-open");
-const navClose = document.querySelector(".navbar-close");
+const iconNavOpen = document.querySelector(".navbar-open");
+const iconNavClose = document.querySelector(".navbar-close");
 
 // Navigation bar mobile
-navOpen.addEventListener("click", function () {
+const openNav = function () {
   navUl.classList.remove("laptop:hidden");
-  navUl.classList.remove("flex");
-  navUl.classList.add("block");
-  navOpen.classList.remove("laptop:block");
-  navClose.classList.remove("hidden");
-});
+  iconNavOpen.classList.remove("laptop:block");
+  iconNavClose.classList.remove("hidden");
+};
 
-navClose.addEventListener("click", function () {
-  navOpen.classList.add("laptop:block");
-  navClose.classList.add("hidden");
+const closeNav = function () {
   navUl.classList.add("laptop:hidden");
-});
+  iconNavOpen.classList.add("laptop:block");
+  iconNavClose.classList.add("hidden");
+};
+
+iconNavOpen.addEventListener("click", openNav);
+iconNavClose.addEventListener("click", closeNav);
